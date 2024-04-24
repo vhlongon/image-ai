225f13750774019d58390740fcb0b8b03582f0f2
vhlongon <vhlongon@gmail.com>
2024-04-24 19:09:57 +0200

    setup up shadcn and theming

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,33 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from '@/components/ui/menubar';
+import { ModeToggle } from './ModeToggle';
+
+export function AppBar() {
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <ModeToggle />
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>New Window </MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Print</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  );
+}
