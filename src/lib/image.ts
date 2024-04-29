cc9bcf6cc893cfc2c4c8d7c94e1478bebb7702e7
vhlongon <vhlongon@gmail.com>
2024-04-30 00:11:37 +0200

    extract utils

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,11 +1,6 @@
 import { RefObject } from 'react';
-import { FixedCropperRef } from 'react-advanced-cropper';
-
-export const getCanvasData = async (canvas: HTMLCanvasElement | null) => {
-  return new Promise((resolve, reject) => {
-    canvas?.toBlob(resolve);
-  });
-};
+import { Coordinates, FixedCropperRef } from 'react-advanced-cropper';
+import { createCanvas, drawImage, drawMask, getCanvasData } from './canvas';
 
 export const getCroppedImageSrc = async (
   cropperRef: RefObject<FixedCropperRef>
@@ -29,4 +24,42 @@ export const editFileName = (filename: string) => {
   const name = filename.substring(0, dotIndex);
   const extension = filename.substring(dotIndex);
   return `${name}-edit${extension}`;
+};
+
+export const getImageData = async (src?: string): Promise<Blob | null> => {
+  if (!src) {
+    return null;
+  }
+
+  const canvas = document.createElement('canvas');
+  await drawImage(canvas, src);
+
+  return getCanvasData(canvas);
+};
+
+export const getMaskData = async (
+  src?: string,
+  selectionRect?: Coordinates | null
+): Promise<Blob | null> => {
+  if (!src || !selectionRect) {
+    return null;
+  }
+
+  const canvas = createCanvas();
+
+  await drawImage(canvas, src);
+  drawMask(canvas, selectionRect);
+
+  return getCanvasData(canvas);
+};
+
+export const dataURLToBlob = (dataURL: string, type: string) => {
+  var binary = atob((dataURL || '').trim());
+  var array = new Array(binary.length);
+
+  for (let i = 0; i < binary.length; i++) {
+    array[i] = binary.charCodeAt(i);
+  }
+
+  return new Blob([new Uint8Array(array)], { type });
 };
