ff9cc2f6d6a25efbb733467ee25f6f11f8d2a9fa
vhlongon <vhlongon@gmail.com>
2024-04-30 00:11:51 +0200

    remove server action and create a api route

---
+++ src/app/images/edit/route.ts
@@ -0,0 +1,37 @@
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+export const POST = async (request: Request, res: Response) => {
+  const formData = await request.formData();
+  const image = formData.get('image') as File;
+  const prompt = formData.get('prompt') as string;
+  const mask = formData.get('mask') as File;
+
+  try {
+    const response = await openai.images.edit({
+      model: 'dall-e-2',
+      image,
+      mask,
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const editImage = response.data[0].b64_json;
+
+    return Response.json({ error: null, image: editImage }, { status: 200 });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error
+        ? error.message
+        : error && typeof error === 'object' && 'response' in error
+        ? JSON.stringify(error.response)
+        : 'Something went wrong generating image';
+
+    return Response.json({ error: errorMessage }, { status: 500 });
+  }
+};
